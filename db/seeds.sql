ee9ce08eaa9609353a53586d7a46e6ccd1badcb5
Jamison Gordon <jgordon@uhin.org>
2019-04-25 15:17:41 -0600

    Changed seeds to have 1 instead of true

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO burgers (burger_name) VALUES ('Bacon and Cheese');
 INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom and swiss', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Marilyn Manson', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Heart attack', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom and swiss', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marilyn Manson', 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Heart attack', 1);
